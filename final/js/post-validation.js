090a2261283d714a7d2c12a2a43a99e2a6f578a3
andy777-droid <hart.andrew117@gmail.com>
2020-08-03 00:16:42 +0200

    post validation start

--- final/js/post-validation.js
+++ final/js/post-validation.js
@@ -1,36 +1,91 @@
 function validateForm() {
-    var suburb = document.forms["post"]["suburb"].value;
-    var subject = document.forms["post"]["subjects"].value;
-    var experience = document.forms["post"]["experience"].value;
+
     var qualification = document.forms["post"]["qualification"].value;
     var education = document.forms["post"]["education"].value;
     var complete = document.forms["post"]["complete"].value;
-    var transport = document.forms["post"]["owntransport"].value;
     var price = document.forms["post"]["price"].value;
     var msg = document.forms["post"]["bio"].value;
-    var age = document.forms["post"]["agerange"].value;
     var radius = document.forms["post"]["radius"].value;
-    var image = document.forms["post"]["test"].value;
 
 
-    alert(suburb + " : " + subject + " : " + experience + " : " + qualification + " : " + education + " : " + complete + " : " + transport + " : " + price + " : " + msg + " : " + age + " : " + radius);
+    //Qualification validation
+    if (qualification == "") {
+        document.getElementById('qualification').style.border = "3px solid red";
+        document.getElementById('qualification').value = "";
+        document.getElementById('qualification').placeholder = "Please enter a valid qualification";
+        document.getElementById('qualification').focus();
+        document.getElementById('qualification').select();
+        return false;
+    } else {
+        document.getElementById('qualification').style.border = "none";
 
+    }
 
+    //Place of education validation
+    if (education == "") {
+        document.getElementById('education').style.border = "3px solid red";
+        document.getElementById('education').value = "";
+        document.getElementById('education').placeholder = "Please enter the place at which your education was done";
+        document.getElementById('education').focus();
+        document.getElementById('education').select();
+        return false;
+    } else {
+        document.getElementById('education').style.border = "none";
 
+    }
 
+    //Year complete validation
+    if (complete > 2020) {
+        document.getElementById('complete').style.border = "3px solid red";
+        document.getElementById('complete').value = "";
+        document.getElementById('complete').placeholder = "Please enter a realistic year";
+        document.getElementById('complete').focus();
+        document.getElementById('complete').select();
+        return false;
+    } else {
+        document.getElementById('complete').style.border = "none";
 
+    }
 
+    //Price per lesson validation
+    if (price == "") {
+        document.getElementById('price').style.border = "3px solid red";
+        document.getElementById('price').value = "";
+        document.getElementById('price').placeholder = "Please enter a price";
+        document.getElementById('price').focus();
+        document.getElementById('price').select();
+        return false;
+    } else {
+        document.getElementById('price').style.border = "none";
 
+    }
+    //Describe yourself validation
+    if (msg == "") {
+        document.getElementById('bio').style.border = "3px solid red";
+        document.getElementById('bio').value = "";
+        document.getElementById('bio').placeholder = "Please describe yourself";
+        document.getElementById('bio').focus();
+        document.getElementById('bio').select();
+        return false;
+    } else {
+        document.getElementById('bio').style.border = "none";
 
+    }
 
+    //Driving radius validation
 
+    if (radius == "") {
+        document.getElementById('radius').style.border = "3px solid red";
+        document.getElementById('radius').value = "";
+        document.getElementById('radius').placeholder = "Please enter a radius";
+        document.getElementById('radius').focus();
+        document.getElementById('radius').select();
+        return false;
+    } else {
+        document.getElementById('radius').style.border = "none";
 
-
-
-
-
-
+    }
 
     return true;
 
-}
+};
--- final/php/post.php
+++ final/php/post.php
@@ -1,4 +1,36 @@
 <?php
-echo '<script type="text/javascript">alert("hello!");</script>';
+//// echo '<script type="text/javascript">alert("hello!");</script>';
 
-?>
+$suburb = $_POST['suburb'];
+$subjects = $_POST['subjects'];
+$experience = $_POST['experience'];
+$qualification = $_POST['qualification'];
+$education = $_POST['education'];
+$complete = $_POST['complete'];
+$owntransport = $_POST['owntransport'];
+$price = $_POST['price'];
+$bio = $_POST['bio'];
+$agerange = $_POST['agerange'];
+$radius = $_POST['radius'];
+$profile = $_POST['profile'];
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "tutoring_website";
+
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db) or die("Unable to connect to database " + $db);
+
+$insert = "INSERT INTO tutor_post (Email, FirstName, LastName, Contacts, Gender, DateOfBirth, Passwords) VALUES ('$email', '$fname', '$lname', '$contact', '$gender', '$dob','$psw')";
+$stmt = $conn->prepare($insert);
+$stmt->execute();
+echo '<script type="text/javascript">alert("Success!");</script>';
+header("Location: http://localhost/tutoring_website/final/html/splash.html");
+$conn->close();
+
+?>  
+
+
