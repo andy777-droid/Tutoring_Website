0f462c44550a16c861d65ec96be17c8b2b078d5f
andy777-droid <hart.andrew117@gmail.com>
2020-08-02 16:59:47 +0200

    user validation

--- final/js/register-validation.js
+++ final/js/register-validation.js
@@ -9,57 +9,91 @@ function validateForm() {
 
     //first name validation
     if (fname == "") {
-        document.getElementById('fname').style.borderColor = "red";
-        document.getElementById('fname').style.placeholder = "Enter a valid name";
+        document.getElementById('fname').style.border = "3px solid red";
+        document.getElementById('fname').value = "";
+        document.getElementById('fname').placeholder = "Please enter a valid name";
         document.getElementById('fname').focus();
         document.getElementById('fname').select();
         return false;
+    } else {
+        document.getElementById('fname').style.border = "none";
     }
 
     //last name validation
     if (lname == "") {
-        document.getElementById('lname').style.borderColor = "red";
+        document.getElementById('lname').style.border = "3px solid red";
+        document.getElementById('lname').value = "";
+        document.getElementById('lname').placeholder = "Please enter a valid surname";
         document.getElementById('lname').focus();
         document.getElementById('lname').select();
         return false;
+    } else {
+        document.getElementById('lname').style.border = "none";
+
     }
 
     //email validation
     if (!email.match(/^\S+@\S+\.\S+$/)) {
-        document.getElementById('email').style.borderColor = "red";
+        document.getElementById('email').style.border = "3px solid red";
+        document.getElementById('email').value = "";
+        document.getElementById('email').placeholder = "Please enter a valid email";
         document.getElementById('email').focus();
         document.getElementById('email').select();
         return false;
+    } else {
+        document.getElementById('email').style.border = "none";
     }
 
     //contact validation
     if (!contact.match(/^[0-9]\d{9}$/)) {
-        document.getElementById('cell').style.borderColor = "red";
+        document.getElementById('cell').style.border = "3px solid red";
+        document.getElementById('cell').value = "";
+        document.getElementById('cell').placeholder = "Please enter a valid cellphone number";
         document.getElementById('cell').focus();
         document.getElementById('cell').select();
         return false;
+    } else {
+        document.getElementById('cell').style.border = "none";
     }
+
 
     //password validation
-    if (password1 == "" || password2 == "") {
-        document.getElementById('idp').style.borderColor = "red";
+    if ((password1 == "") || (password2 == "")) {
+        document.getElementById('idp').style.border = "3px solid red";
+        document.getElementById('idp').value = "";
+        document.getElementById('idp2').value = "";
+        document.getElementById('idp').placeholder = "Please enter a password";
         document.getElementById('idp').focus();
         document.getElementById('idp').select();
-        document.getElementById('idp2').style.borderColor = "red";
+        document.getElementById('idp2').style.border = "3px solid red";
         return false;
     } else {
-        if (password1 !== password2) {
-            document.getElementById('idp').style.borderColor = "red";
-            document.getElementById('idp2').style.borderColor = "red";
+        if (document.forms["reg"]["idp"].value.length < 6) {
+            document.getElementById('idp').style.border = "3px solid red";
             document.getElementById('idp').value = "";
             document.getElementById('idp2').value = "";
-            check = false;
+            document.getElementById('idp').placeholder = "Password must be at least 6 characters long";
+            document.getElementById('idp').focus();
+            document.getElementById('idp').select();
+            document.getElementById('idp2').style.border = "3px solid red";
             return false;
-
+        } else {
+            if (password1 != password2) {
+                document.getElementById('idp').style.border = "3px solid red";
+                document.getElementById('idp2').style.border = "3px solid red";
+                document.getElementById('idp').value = "";
+                document.getElementById('idp2').value = "";
+                document.getElementById('idp2').placeholder = "Passwords must match";
+                return false;
+            } else {
+                document.getElementById('idp').style.border = "none";
+                document.getElementById('idp2').style.border = "none";
+            }
         }
+
     }
 
-    alert("Form submitted");
+    alert("User registered successfully!");
     return true;
 
 }
--- final/php/register.php
+++ final/php/register.php
@@ -4,6 +4,8 @@
   $lname = $_POST['lname'];
   $email = $_POST['email'];
   $contact = $_POST['cell'];
+  $gender = $_POST['gender'];
+  $dob = $_POST['dob'];
   $psw = $_POST['idp'];
   $cpsw = $_POST['idp2'];
 
@@ -16,7 +18,7 @@
   // Create connection
   $conn = new mysqli($host, $user, $pass, $db) or die("Unable to connect to database " + $db);
 
-  $insert = "INSERT INTO user_login (Email, FirstName, LastName, Contacts, Passwords) VALUES ('$email', '$fname', '$lname', '$contact', '$psw')";
+  $insert = "INSERT INTO user_login (Email, FirstName, LastName, Contacts, Gender, DateOfBirth, Passwords) VALUES ('$email', '$fname', '$lname', '$contact', '$gender', '$dob','$psw')";
   $stmt = $conn->prepare($insert);
   $stmt->execute();
   $conn->close();
