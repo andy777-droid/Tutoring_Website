a75639c3dcbf2f65dc1ec05b7aec399a778bb964
andy777-droid <hart.andrew117@gmail.com>
2020-08-05 00:31:37 +0200

    parse username

--- final/php/login.php
+++ final/php/login.php
@@ -1,30 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "tutoring_website";
+<head>
+    <script src="../htdocs/Tutoring_Website/final/js/user.js"></script>
+    </body>
 
-$conn = new mysqli($host, $user, $pass, $db);
+</head>
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$email = $_POST['email'];
-$password = $_POST['psw'];
+<body>
+    <?php
 
-$result = mysqli_query($conn, "select * from user_login where Email = '" . $email . "' and Passwords = '" . $password . "'") or die("Unable to read data");
-$resultCheck = mysqli_num_rows($result);
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "tutoring_website";
 
-if ($resultCheck > 0) {
-    $rows = mysqli_fetch_assoc($result);
-    if (($rows['Email'] == $email) && ($rows['Passwords'] == $password)) {
-        header("Location: http://localhost/tutoring_website/final/html/post.html");
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $email = $_POST['email'];
+    $password = $_POST['psw'];
+
+    $result = mysqli_query($conn, "select * from user_login where Email = '" . $email . "' and Passwords = '" . $password . "'") or die("Unable to read data");
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+        $rows = mysqli_fetch_assoc($result);
+        if (($rows['Email'] == $email) && ($rows['Passwords'] == $password)) {
+
+            header("Location: http://localhost/tutoring_website/final/html/post.html?user=".$email);
+        }
+    } else {
+        header("Location: http://localhost/tutoring_website/final/html/login.html");
     };
-} else {
-    header("Location: http://localhost/tutoring_website/final/html/login.html");
-};
 
-$conn->close();
+    $conn->close();
 
-?>
+    ?>
+
+
+</html>
